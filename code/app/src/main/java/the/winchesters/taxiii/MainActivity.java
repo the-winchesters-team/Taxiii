ebe06d382806593974d0ea7cbaa9d1af930e36ba
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-04-05 16:57:10 +0000

    added homescreen

--- code/app/src/main/java/the/winchesters/taxiii/MainActivity.java
+++ code/app/src/main/java/the/winchesters/taxiii/MainActivity.java
@@ -3,12 +3,20 @@ package the.winchesters.taxiii;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_main);
     }
 }
