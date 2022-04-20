2c1c820025f72b8c44aedddecab53925e9009a8d
HamzaBenyazid <benyzidhamza969@gmail.com>
2022-04-20 13:41:19 +0000

    add createRide in RideController

--- Taxiii/src/main/java/ma/ensias/winchesters/controller/RideController.java
+++ Taxiii/src/main/java/ma/ensias/winchesters/controller/RideController.java
@@ -1,4 +1,19 @@
 package ma.ensias.winchesters.controller;
 
+import ma.ensias.winchesters.dto.RideDto;
+import ma.ensias.winchesters.model.Ride;
+import ma.ensias.winchesters.service.RideService;
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class RideController {
+    private final RideService rideService;
+
+    public RideController(RideService rideService) {
+        this.rideService = rideService;
+    }
+
+    @PostMapping
+    private Ride createRide(RideDto ride){
+        return rideService.createRide(ride);
+    }
 }
