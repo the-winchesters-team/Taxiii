d5ff31042ebb6641023ea71acb7d0f007621cd09
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-04-22 15:30:03 +0000

    added google maps

---
+++ code/app/src/main/java/the/winchesters/taxiii/MapsActivity.java
@@ -0,0 +1,40 @@
+package the.winchesters.taxiii;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import the.winchesters.taxiii.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng morocco = new LatLng(33.589886, 	-7.603869);
+        mMap.addMarker(new MarkerOptions().position(morocco).title("Morocco"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(morocco));
+    }
+}
