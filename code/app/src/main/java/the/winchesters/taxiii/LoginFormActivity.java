ef2dae9f6310947d7efb232292d77f366098dbaf
HamzaBenyazid <benyzidhamza969@gmail.com>
2022-04-23 00:35:04 +0000

    save token in sharedPreferences and link signUp activity with API

--- code/app/src/main/java/the/winchesters/taxiii/LoginFormActivity.java
+++ code/app/src/main/java/the/winchesters/taxiii/LoginFormActivity.java
@@ -1,11 +1,19 @@
 package the.winchesters.taxiii;
 
+import static android.app.PendingIntent.getActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONException;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -20,8 +28,6 @@ import the.winchesters.taxiii.retrofit.RetrofitService;
 public class LoginFormActivity extends AppCompatActivity {
 
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +37,7 @@ public class LoginFormActivity extends AppCompatActivity {
 
     private void initialiseComponents() {
         TextView emailTV = (TextView) findViewById(R.id.editTextTextEmailAddress);
-        TextView passwordTV =(TextView)  findViewById(R.id.editTextTextPassword);
+        TextView passwordTV = (TextView) findViewById(R.id.editTextTextPassword);
         Button signinButton = (Button) findViewById(R.id.signinButton);
 
         RetrofitService retrofitService = new RetrofitService();
@@ -45,14 +51,27 @@ public class LoginFormActivity extends AppCompatActivity {
             loginFormData.setUsername(username);
             loginFormData.setPassword(password);
 
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+            SharedPreferences.Editor editor = preferences.edit();
+
             apiEndPoints.login(loginFormData)
                     .enqueue(new Callback() {
                         @Override
                         public void onResponse(Call call, Response response) {
-                            String msg = response.headers().get("Authorization");
-                            Toast.makeText(LoginFormActivity.this, msg, Toast.LENGTH_SHORT).show();
-                        }
+                            if (response.code()==200){
+                                //https://stackoverflow.com/questions/3624280/how-to-use-sharedpreferences-in-android-to-store-fetch-and-edit-values
+                                //Save token in shared preferences
+                                String saveToken = response.headers().get("Authorization");
+                                editor.putString("Authorization", saveToken);
+                                Log.i("Login", saveToken);
+                                editor.apply();
+                                Toast.makeText(LoginFormActivity.this, "success", Toast.LENGTH_SHORT).show();
 
+                            }else{
+                                Toast.makeText(LoginFormActivity.this, String.format("error. code : %s",response.code()), Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
                         @Override
                         public void onFailure(Call call, Throwable t) {
                             Toast.makeText(LoginFormActivity.this, "Failure", Toast.LENGTH_SHORT).show();
