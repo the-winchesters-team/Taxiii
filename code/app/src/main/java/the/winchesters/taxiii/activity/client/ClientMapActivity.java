91279352a4230f8fcbdaa9c1d614b7e2bab0e6ce
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-05-27 20:05:15 +0100

    added taxi driver mark

--- code/app/src/main/java/the/winchesters/taxiii/activity/client/ClientMapActivity.java
+++ code/app/src/main/java/the/winchesters/taxiii/activity/client/ClientMapActivity.java
@@ -1,38 +1,41 @@
 package the.winchesters.taxiii.activity.client;
 
-import static android.content.ContentValues.TAG;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentActivity;
 
 import com.firebase.geofire.GeoFire;
 import com.firebase.geofire.GeoLocation;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.location.LocationRequest;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.List;
 import java.util.Objects;
 
 import the.winchesters.taxiii.R;
+import the.winchesters.taxiii.activity.NavigationBarActivity;
 import the.winchesters.taxiii.databinding.ActivityTaxiDriverMapBinding;
 
 public class ClientMapActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
@@ -52,7 +55,6 @@ public class ClientMapActivity extends FragmentActivity implements OnMapReadyCal
         setContentView(binding.getRoot());
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        assert mapFragment != null;
         mapFragment.getMapAsync(this);
     }
 
@@ -63,29 +65,45 @@ public class ClientMapActivity extends FragmentActivity implements OnMapReadyCal
             return;
         buildClient();
         map.setMyLocationEnabled(true);
+        // set to driver's mark
+        // get latlng
+        Bundle b = getIntent().getExtras();
+        if (b == null) return;
+        if (b.getDouble("lat") == 0.0 || b.getDouble("long") == 0.0) return;
+        googleMap.addMarker(new MarkerOptions()
+                .position(
+                        new LatLng(b.getDouble("lat"),b.getDouble("long"))
+                )
+                .title("Taxi choisi"));
+        map.moveCamera(
+                CameraUpdateFactory.newLatLng(
+                        new LatLng(b.getDouble("lat"),b.getDouble("long"))
+                )
+        );
+    }
 
+    private synchronized void buildClient() {
+        GoogleApiClient.Builder builder = new GoogleApiClient.Builder(this);
+        builder.addApi(LocationServices.API);
+        builder.addConnectionCallbacks(this);
+        builder.addOnConnectionFailedListener(this);
+        googleApiClient = builder.build();
+        googleApiClient.connect();
     }
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
         lastKnownLocation = location;
         Log.d("debug", location.toString());
-        map.moveCamera(
-                CameraUpdateFactory.newLatLng(
-                        new LatLng(
-                                lastKnownLocation.getLatitude(),
-                                lastKnownLocation.getLongitude()
-                        )
-                )
-        );
-        map.animateCamera(CameraUpdateFactory.zoomBy(15));
-        // get current user's id
-        String currentUser = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
-        // get the reference to the "DriverIsAvailable" db
-        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("DriverIsAvailable");
-        GeoLocation geoLocation = new GeoLocation(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
-        GeoFire geoFire = new GeoFire(dbRef);
-        geoFire.setLocation(currentUser, geoLocation, (key, error) -> Log.e(TAG, "GeoFire Complete"));
+//        map.moveCamera(
+//                CameraUpdateFactory.newLatLng(
+//                        new LatLng(
+//                                lastKnownLocation.getLatitude(),
+//                                lastKnownLocation.getLongitude()
+//                        )
+//                )
+//        );
+//        map.animateCamera(CameraUpdateFactory.zoomTo(10));
     }
 
 
@@ -93,18 +111,18 @@ public class ClientMapActivity extends FragmentActivity implements OnMapReadyCal
     public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
-
     @Override
     public void onConnected(@Nullable Bundle bundle) {
         locationRequest = new LocationRequest()
                 //every second
                 .setInterval(1000)
+                .setFastestInterval(1000)
                 // high accuracy because we need the drivers accurate location
-                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-        ;
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         if (!checkLocationPermission())
             return;
         LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+
     }
 
     @Override
@@ -116,7 +134,6 @@ public class ClientMapActivity extends FragmentActivity implements OnMapReadyCal
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
-
     public boolean checkLocationPermission() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
@@ -141,28 +158,5 @@ public class ClientMapActivity extends FragmentActivity implements OnMapReadyCal
         } else {
             return true;
         }
-    }
-
-    private synchronized void buildClient() {
-        googleApiClient = new GoogleApiClient
-                .Builder(this)
-                .addApi(LocationServices.API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
-        googleApiClient.connect();
-    }
-
-    @Override
-    protected void onStop() {
-        // when no longer tracked
-        super.onStop();
-        // get current user's id
-        String currentUser = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
-        // get the reference to the "DriverIsAvailable" db
-        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("DriverIsAvailable");
-        GeoFire geoFire = new GeoFire(dbRef);
-        geoFire.removeLocation(currentUser);
-
     }
 }
