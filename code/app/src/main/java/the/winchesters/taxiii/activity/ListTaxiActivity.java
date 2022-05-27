98259bfb6267b80042203a9c263c676430a807d7
HamzaBenyazid <hamzabyazid@gmail.com>
2022-05-27 20:40:30 +0100

    add startTaxiDriverListener() method

--- code/app/src/main/java/the/winchesters/taxiii/activity/ListTaxiActivity.java
+++ code/app/src/main/java/the/winchesters/taxiii/activity/ListTaxiActivity.java
@@ -1,20 +1,100 @@
 package the.winchesters.taxiii.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.content.ContentValues.TAG;
 
 import android.os.Bundle;
+import android.util.Log;
 
-import java.util.List;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
 
 import the.winchesters.taxiii.R;
 import the.winchesters.taxiii.model.TaxiDriver;
 
 public class ListTaxiActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private Map<String, TaxiDriver> taxiDrivers;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_taxi);
+        mAuth = FirebaseAuth.getInstance();
+        startTaxiDriverListener();
     }
 
+    public void startTaxiDriverListener() {
+        DatabaseReference driverIsAvailableRef = FirebaseDatabase.getInstance().getReference("DriverIsAvailable");
+        driverIsAvailableRef.addChildEventListener(new ChildEventListener() {
+            String taxiDriverId;
+            TaxiDriver taxiDriver;
+            LatLng location;
+
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                taxiDriverId = snapshot.getKey();
+                location = snapshot.getValue(LatLng.class);
+                DatabaseReference driverRef = FirebaseDatabase.getInstance()
+                        .getReference("User")
+                        .child("TaxiDriver")
+                        .child(taxiDriverId);
+                driverRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        taxiDriver = snapshot.getValue(TaxiDriver.class);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Log.w(TAG, "loadPost:onCancelled", error.toException());
+                    }
+                });
+                taxiDriver.setLocation(location);
+
+                taxiDrivers.put(taxiDriverId, taxiDriver);
+                Log.i(TAG, taxiDriver.toString());
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                taxiDriverId = snapshot.getKey();
+                location = snapshot.getValue(LatLng.class);
+                taxiDriver = taxiDrivers.get(taxiDriverId);
+                assert taxiDriver != null;
+                taxiDriver.setLocation(location);
+                taxiDrivers.put(taxiDriverId, taxiDriver);
+                Log.i(TAG, taxiDriver.toString());
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+                taxiDriverId = snapshot.getKey();
+                taxiDrivers.remove(taxiDriverId);
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w(TAG, "loadPost:onCancelled", error.toException());
+            }
+        });
+    }
 }
--- code/app/src/main/java/the/winchesters/taxiii/model/TaxiDriver.java
+++ code/app/src/main/java/the/winchesters/taxiii/model/TaxiDriver.java
@@ -1,7 +1,23 @@
 package the.winchesters.taxiii.model;
 
+import com.google.android.gms.maps.model.LatLng;
+
 public class TaxiDriver extends User {
+    private LatLng location;
     public TaxiDriver(String username, String firstName, String lastName, String phoneNumber) {
         super(username, firstName, lastName, phoneNumber);
+    }
+
+    public TaxiDriver(String username, String firstName, String lastName, String phoneNumber, LatLng location) {
+        super(username, firstName, lastName, phoneNumber);
+        this.location = location;
+    }
+
+    public LatLng getLocation() {
+        return location;
+    }
+
+    public void setLocation(LatLng location) {
+        this.location = location;
     }
 }
